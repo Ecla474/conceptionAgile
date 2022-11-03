343328e756309d49f0ba5a0d0d324b86e8a21047
Marmotte69 <mathieu.allard3@gmail.com>
2022-11-03 20:15:53 +0100

    MVC sur Player

---
+++ README.md
@@ -0,0 +1,4 @@
+# Conception agile de projets informatiques et génie logiciel
+## TP3 - Refactoring et design-patterns
+
+Implémentation du sujet de TP disponible à l'adresse [https://valentin.lachand.net/teaching/2022/conception-projet-agile-2022-tp-3](https://valentin.lachand.net/teaching/2022/conception-projet-agile-2022-tp-3)
---
+++ src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/Field.java
@@ -0,0 +1,114 @@
+package fr.icom.info.m1.balleauprisonnier_mvn;
+
+
+import java.util.ArrayList;
+
+import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+
+/**
+ * Classe gerant le terrain de jeu.
+ * 
+ */
+public class Field extends Canvas {
+	
+	/** Joueurs */
+	Player [] joueurs = new Player[2];
+	/** Couleurs possibles */
+	String[] colorMap = new String[] {"blue", "green", "orange", "purple", "yellow"};
+	/** Tableau traçant les evenements */
+    ArrayList<String> input = new ArrayList<String>();
+    
+
+    final GraphicsContext gc;
+    final int width;
+    final int height;
+    
+    /**
+     * Canvas dans lequel on va dessiner le jeu.
+     * 
+     * @param scene Scene principale du jeu a laquelle on va ajouter notre Canvas
+     * @param w largeur du canvas
+     * @param h hauteur du canvas
+     */
+	public Field(Scene scene, int w, int h){
+		super(w, h); 
+		width = w;
+		height = h;
+		
+		/** permet de capturer le focus et donc les evenements clavier et souris */
+		this.setFocusTraversable(true);
+		
+        gc = this.getGraphicsContext2D();
+        
+        /** On initialise le terrain de jeu */
+    	joueurs[0] = new Player(gc, colorMap[0], w/2, h-50, "bottom", scene);
+    	//joueurs[0].display();
+
+    	joueurs[1] = new Player(gc, colorMap[1], w/2, 20, "top", scene);
+    	//joueurs[1].display();
+
+
+	    /** 
+	     * Event Listener du clavier 
+	     * quand une touche est pressee on la rajoute a la liste d'input
+	     *   
+	     */
+	    this.setOnKeyPressed(
+	    		new EventHandler<KeyEvent>()
+	    	    {
+	    	        public void handle(KeyEvent e)
+	    	        {
+	    	            String code = e.getCode().toString();
+	    	            // only add once... prevent duplicates
+	    	            if ( !input.contains(code) )
+	    	                input.add( code );
+	    	        }
+	    	    });
+
+	    /** 
+	     * Event Listener du clavier 
+	     * quand une touche est relachee on l'enleve de la liste d'input
+	     *   
+	     */
+	    this.setOnKeyReleased(
+			new EventHandler<KeyEvent>(){
+				public void handle(KeyEvent e){
+					String code = e.getCode().toString();
+					input.remove( code );
+				}
+			}
+		);
+	    
+	    /** 
+	     * 
+	     * Boucle principale du jeu
+	     * 
+	     * handle() est appelee a chaque rafraichissement de frame
+	     * soit environ 60 fois par seconde.
+	     * 
+	     */
+	    new AnimationTimer(){
+	        public void handle(long currentNanoTime){	 
+	            // On nettoie le canvas a chaque frame
+	            gc.setFill( Color.LIGHTGRAY);
+	            gc.fillRect(0, 0, width, height);
+	        	
+	            // Deplacement et affichage des joueurs
+				for(int i = 0; i < joueurs.length; i++){
+					joueurs[i].controlleur();
+				}
+	    	}
+	     }.start(); // On lance la boucle de rafraichissement 
+	     
+	}
+
+	public Player[] getJoueurs() {
+		return joueurs;
+	}
+}
